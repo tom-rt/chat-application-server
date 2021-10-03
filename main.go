007bc22ee248cd333d8313cb4c50fc58d22ad648
Thomas Raout <thomas.raout@neoxia.com>
2021-10-03 20:54:49 +0200

    WIP Handling room limits connections and disconnections gently

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gorilla/websocket"
 )
@@ -20,34 +23,96 @@ type Session struct {
 	Connection *websocket.Conn
 }
 
-var chatRoom []Session
-
-func connect(connection *websocket.Conn, nickname string, messageType int) {
-	var newConnection Session = Session{Nickname: string(nickname), Connection: connection}
-	chatRoom = append(chatRoom, newConnection)
-
-	err := connection.WriteMessage(messageType, []byte("Connection approved"))
-
-	if err != nil {
-		log.Println("Error writing message:", err)
-	}
+type ConnectionResponse struct {
+	IsAllowed bool
+	Message   string
 }
 
-// Pass struct as a pointer instead ?
-func broadcastMessage(connection *websocket.Conn, message messageStruct, messageType int) {
-	for _, session := range chatRoom {
-		err := session.Connection.WriteMessage(messageType, []byte(message.Message))
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-}
-
-type messageStruct struct {
+type MessageStruct struct {
 	Connection    bool
 	Disconnection bool
 	Nickname      string
 	Message       string
+}
+
+var chatRoom []Session
+
+func isConnectionAllowed(nickname string) (bool, string) {
+	if len(chatRoom) >= 10 {
+		return false, "Connection denied: the server is full, try again later."
+	}
+
+	for _, sess := range chatRoom {
+		if sess.Nickname == nickname {
+			return false, "Connection denied: this nickname is already taken."
+		}
+	}
+	return true, "Connection successful."
+}
+
+func disconnect(connection *websocket.Conn, nickname string, messageType int) {
+	for i, session := range chatRoom {
+		if session.Nickname == nickname {
+			fmt.Println("INDEX", i)
+			if len(chatRoom) == 1 {
+				chatRoom = nil
+			} else if i == 0 {
+				chatRoom[i] = chatRoom[len(chatRoom)-1]
+				chatRoom = chatRoom[:len(chatRoom)-1]
+			} else {
+				chatRoom[i] = chatRoom[0]
+				chatRoom = chatRoom[:len(chatRoom)-1]
+			}
+			broadcastMessage(connection, nickname, "*"+nickname+" left the chat*", messageType)
+		}
+	}
+}
+
+func connect(connection *websocket.Conn, nickname string, messageType int) {
+	isAllowed, errMessage := isConnectionAllowed(nickname)
+	var response ConnectionResponse = ConnectionResponse{IsAllowed: isAllowed, Message: errMessage}
+
+	marshaledPayload, error := json.Marshal(response)
+	if error != nil {
+		fmt.Println("error marshalling", error)
+	}
+	err := connection.WriteMessage(websocket.TextMessage, []byte(string(marshaledPayload)))
+	if err != nil {
+		log.Println("Error writing message.", err)
+		return
+	}
+
+	if isAllowed {
+		// Pushing the connection in the room
+		var newConnection Session = Session{Nickname: string(nickname), Connection: connection}
+		chatRoom = append(chatRoom, newConnection)
+		broadcastMessage(connection, nickname, "*Has joined the chat*", messageType)
+
+	} else {
+		fmt.Printf("Connection not allowed")
+	}
+}
+
+// Pass struct as a pointer instead ?
+func broadcastMessage(connection *websocket.Conn, nickname string, message string, messageType int) {
+	for _, session := range chatRoom {
+		if session.Nickname != nickname {
+			err := session.Connection.WriteMessage(messageType, []byte(nickname+": "+message))
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+}
+
+func disconnectRoom() {
+	for _, session := range chatRoom {
+		err := session.Connection.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "Closing client: the server has shut down."))
+		if err != nil {
+			fmt.Println("Error on disconnecting user:", err)
+		}
+	}
+	os.Exit(0)
 }
 
 func handleSession(w http.ResponseWriter, r *http.Request) {
@@ -58,6 +123,17 @@ func handleSession(w http.ResponseWriter, r *http.Request) {
 	}
 	defer connection.Close()
 
+	// CATCHING ^C
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			if sig == syscall.SIGINT {
+				disconnectRoom()
+			}
+		}
+	}()
+
 	for {
 		messageType, message, err := connection.ReadMessage()
 		if err != nil {
@@ -65,14 +141,15 @@ func handleSession(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 
-		var resp messageStruct
+		var resp MessageStruct
 		json.Unmarshal(message, &resp)
 
 		if resp.Connection {
 			connect(connection, resp.Nickname, messageType)
 		} else if resp.Disconnection {
+			disconnect(connection, resp.Nickname, messageType)
 		} else {
-			broadcastMessage(connection, resp, messageType)
+			broadcastMessage(connection, resp.Nickname, resp.Message, messageType)
 		}
 
 		if err != nil {
